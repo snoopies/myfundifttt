53c32ed3f9741a1d77685abedc7ff0416e322acc
snoopies <snoopies@tom.com>
2019-02-09 22:08:07 +0800

    20190209

--- app.py
+++ app.py
@@ -77,12 +77,12 @@ def get_fund():
 
 @app.route('/')
 def homepage():
+    http_post2()
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
 if __name__ == '__main__':
