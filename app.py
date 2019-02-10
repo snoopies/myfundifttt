0cd5a359a47566853dffbeb4d829d69aa0aa68ff
snoopies <snoopies@tom.com>
2019-02-10 19:55:31 +0800

    20190210

--- app.py
+++ app.py
@@ -84,10 +84,11 @@ def homepage():
     http_post2()
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+     return request.args
+#    return """
+#    <h1>Hello heroku</h1>
+#    <p>It is currently {time}.</p>
+#    """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
