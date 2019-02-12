a0206ff3b4443c191c5e24bfb4b4ea4df2e2b216
snoopies <snoopies@tom.com>
2019-02-12 20:23:40 +0800

    20190212

--- app.py
+++ app.py
@@ -101,6 +101,7 @@ def funds():
     if request.args.get("fundscode") == None:
         return "Error Request!"
     funds=request.args.get("fundscode").split(",")
+    return funds
     for fund in funds:
         data = get_fund(fund)
         http_post2(data)
